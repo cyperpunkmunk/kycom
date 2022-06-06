003ca09143b5b8598e54e2911c1572e2cfe378c0
cyperpunkmunk <kendgibsonjr@gmail.com>
2022-06-06 12:54:47 -0500

    ice

--- src/NFT/NFT.js
+++ src/NFT/NFT.js
@@ -6,7 +6,20 @@ export default function NftPage() {
 
     return (
       <div className='nftBody'>
-  
+        <div className='sec1'>
+          <div className='item'>
+              <p> section 1</p>
+          </div>
+          <div className="item">
+              <p> section 2 </p>
+          </div>
+          <div className="item">
+              <p> section 3 </p>
+          </div>
+
+          
+
+        </div>
       </div>
     );
 }
